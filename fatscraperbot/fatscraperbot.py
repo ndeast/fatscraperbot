4b09faf6b42ff5758267daa5ac088552229bbcbb
Nik <nik@ndeast.com>
2020-06-10 16:41:18 -0400

    read new releases from a log instead of storing counts

--- fatscraperbot/fatscraperbot.py
+++ fatscraperbot/fatscraperbot.py
@@ -2,7 +2,6 @@ import tokens as tok
 import json
 import os
 
-rec_count = 0
 recs = []
 
 def get_last_n_recs(n, recs):
@@ -13,29 +12,31 @@ def get_last_n_recs(n, recs):
 
 def parse_recs():
     if os.path.exists(tok.JSONFILE):
-        with open(tok.JSONFILE, 'r') as f, open(tok.COUNT, 'w') as c:
+        with open(tok.JSONFILE, 'r') as f:
             recs = json.load(f)
-            c.write(str(len(recs)))
     else:
         print("file not found")
     return recs
 
-def updated_rec_count():
-    count = 0
-    if os.path.exists(tok.COUNT):
-        with open(tok.COUNT, 'r') as c:
-            count = int(c.readline().strip())
-    return count
+def get_new_rec_ids():
+    new_releases = []
+    if os.path.exists(tok.NEWRELEASES):
+        with open(tok.NEWRELEASES, 'r') as c:
+            new_releases = [int(l.rstrip("\n")) for l in c]
+    return new_releases
 
-def new_release(recs, rec_count):
-    recs = parse_recs()
-    new_rec_count = updated_rec_count()
-    num_new_recs = (new_rec_count - rec_count)
-    rec_count = new_rec_count
-    print(get_last_n_recs(num_new_recs, recs))
+def handle_new_releases(recs):
+    new_rec_ids = get_new_rec_ids()
+    for rec_id in new_rec_ids:
+        print(format_release(recs[rec_id]))
 
-rec_count = updated_rec_count() - 1
-new_release(recs, rec_count)
-# recs = parse_recs()
-# if rec_count != len(recs):
-#     print(get_last_n_recs((len(recs) - rec_count), recs))
+def format_release(rec):
+    return (
+        f"Title: {rec.get('title')}\n"
+        f"Artist: {rec.get('artist')}\n"
+        f"Release Date: {rec.get('release_date')}\n"
+        f"Link: {rec.get('link')}\n"
+    )
+
+recs = parse_recs()
+handle_new_releases(recs)
