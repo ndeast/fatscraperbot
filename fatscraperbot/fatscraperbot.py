7c6426b72c79589abf7b71d43657a48e70945f42
Nik <nik@ndeast.com>
2020-06-10 15:15:50 -0400

    update on new releases

--- fatscraperbot/fatscraperbot.py
+++ fatscraperbot/fatscraperbot.py
@@ -1,5 +1,9 @@
+import tokens as tok
 import json
 import os
+
+rec_count = 0
+recs = []
 
 def get_last_n_recs(n, recs):
     if n > 0:
@@ -7,9 +11,9 @@ def get_last_n_recs(n, recs):
     else:
         return []
 
-def parse_recs(file):
-    if os.path.exists(file):
-        with open(file, 'r') as f, open('count', 'w') as c:
+def parse_recs():
+    if os.path.exists(tok.JSONFILE):
+        with open(tok.JSONFILE, 'r') as f, open(tok.COUNT, 'w') as c:
             recs = json.load(f)
             c.write(str(len(recs)))
     else:
@@ -18,15 +22,20 @@ def parse_recs(file):
 
 def updated_rec_count():
     count = 0
-    if os.path.exists('count'):
-        with open('count', 'r') as c:
+    if os.path.exists(tok.COUNT):
+        with open(tok.COUNT, 'r') as c:
             count = int(c.readline().strip())
     return count
 
+def new_release(recs, rec_count):
+    recs = parse_recs()
+    new_rec_count = updated_rec_count()
+    num_new_recs = (new_rec_count - rec_count)
+    rec_count = new_rec_count
+    print(get_last_n_recs(num_new_recs, recs))
 
-rec_count = 32
-
-recs = parse_recs('UpcomingRecords.json')
-
-new_recs = (updated_rec_count() - rec_count)
-print(get_last_n_recs(new_recs, recs))
+rec_count = updated_rec_count() - 1
+new_release(recs, rec_count)
+# recs = parse_recs()
+# if rec_count != len(recs):
+#     print(get_last_n_recs((len(recs) - rec_count), recs))
