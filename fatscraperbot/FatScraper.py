7e352d82da9f95ded27bc7e290701356c68b2d4d
Nik <nik@ndeast.com>
2020-06-11 17:31:33 -0400

    move Fatscraper methods into a class, functional telegram bot

---
+++ fatscraperbot/FatScraper.py
@@ -0,0 +1,64 @@
+import os
+import json
+class FSData:
+
+    def __init__(self, datafile, countfile):
+        self.datafile = datafile
+        self.countfile = countfile
+        self.recs = self.__parse_recs()
+        self.last_five_recs = self.__print_last_five()
+        self.latest_release = self.format_release(self.recs[-1])
+        self.new_recs = 0
+
+    def __get_last_n_recs(self, n, recs):
+        if n > 0:
+            return self.recs[-n:]
+        else:
+            return []
+
+    def __print_last_five(self):
+        l = len(self.recs) - 1 
+        str = ""
+        for i in range(l, (l - 5), -1):
+            str += self.format_release(self.recs[i])
+        return str
+
+    def __parse_recs(self):
+        temp_recs = []
+        if os.path.exists(self.datafile):
+            with open(self.datafile, 'r') as f:
+                temp_recs = json.load(f)
+        else:
+            print("file not found")
+        return temp_recs
+
+    def __get_num_new_recs(self):
+        new_releases = 0
+        if os.path.exists(self.countfile):
+            with open(self.countfile, 'r') as c:
+                num = c.readline().rstrip()
+                new_releases = int(num) if num.isdigit() else 0
+        return new_releases
+
+    def __handle_new_releases(self):
+        tempstr = ""
+        self.recs = self.__parse_recs()
+        new_recs = self.__get_last_n_recs(self.__get_num_new_recs(), self.recs)
+        for rec in new_recs:
+            tempstr += self.format_release(rec)
+        os.remove(self.countfile)
+        self.latest_release = tempstr
+
+    def format_release(self, rec):
+        return (
+            f"Title: {rec.get('title')}\n"
+            f"Artist: {rec.get('artist')}\n"
+            f"Release Date: {rec.get('release_date')}\n"
+            f"Link: {rec.get('link')}\n\n"
+        )
+
+    def update(self):
+        self.__handle_new_releases()
+
+    def new_record_released(self):
+        return self.__get_num_new_recs()
--- fatscraperbot/fatscraperbot.py
+++ fatscraperbot/fatscraperbot.py
@@ -1,4 +1,5 @@
 import tokens as tok
+from FatScraper import FSData
 import json
 import os
 import time
@@ -10,58 +11,10 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-
-recs = []
-last_five_recs = ""
-latest_release = ""
 new_recs = 0
 poll = 10
 
-def get_last_n_recs(n, recs):
-    if n > 0:
-        return recs[-n:]
-    else:
-        return []
-
-def print_last_five(recs):
-    l = len(recs) - 1 
-    str = ""
-    for i in range(l, (l - 5), -1):
-        str += format_release(recs[i])
-    return str
-
-def parse_recs():
-    temp_recs = []
-    if os.path.exists(tok.JSONFILE):
-        with open(tok.JSONFILE, 'r') as f:
-            temp_recs = json.load(f)
-    else:
-        print("file not found")
-    return temp_recs
-
-def get_num_new_recs():
-    new_releases = 0
-    if os.path.exists(tok.NEWRELEASES):
-        with open(tok.NEWRELEASES, 'r') as c:
-            num = c.readline().rstrip()
-            new_releases = int(num) if num.isdigit() else 0
-    return new_releases
-
-def handle_new_releases(recs, new_recs):
-    tempstr = ""
-    new_recs = get_last_n_recs(get_num_new_recs(), recs)
-    for rec in new_recs:
-        tempstr += format_release(rec)
-    # os.remove(tok.NEWRELEASES)
-    return tempstr
-
-def format_release(rec):
-    return (
-        f"Title: {rec.get('title')}\n"
-        f"Artist: {rec.get('artist')}\n"
-        f"Release Date: {rec.get('release_date')}\n"
-        f"Link: {rec.get('link')}\n\n"
-    )
+fs = FSData(tok.JSONFILE, tok.NEWRELEASES)
 
 # Telegram command handlers
 def start(update, context):
@@ -69,7 +22,7 @@ def start(update, context):
     update.message.reply_text("Working")
 
 def last(update, context):
-    update.message.reply_text(latest_release)
+    update.message.reply_text(fs.latest_release)
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -80,6 +33,7 @@ def main():
     updater = Updater(tok.TELEGRAMBOT, use_context=True)
 
     dp = updater.dispatcher
+    bot = updater.bot
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("last", last))
@@ -93,20 +47,15 @@ def main():
         if tr == poll:
             tr = 0
 
-            if (new_recs := get_num_new_recs()):
+            if (fs.new_record_released()):
                 logger.info("New Release!")
-                recs = parse_recs()
-                latest_release = handle_new_releases(recs, new_recs)
+                fs.update()
+                bot.send_message(tok.ADMINCHATID[0], fs.latest_release)
         
         time.sleep(10)
         tr += 10
 
     updater.idle()
-
-
-recs = parse_recs()
-last_five_recs = print_last_five(recs)
-latest_release = format_release(recs[-1])
 
 if __name__ == '__main__':
     main()
